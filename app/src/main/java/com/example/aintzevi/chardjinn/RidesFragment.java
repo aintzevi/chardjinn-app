c371334528587df8c5697b9eb93df6781a47655b
maxiklinke <maxi.klinke@googlemail.com>
2018-03-16 10:12:49 +0100

    main fragments added.

---
+++ app/src/main/java/com/example/aintzevi/chardjinn/RidesFragment.java
@@ -0,0 +1,66 @@
+package com.example.aintzevi.chardjinn;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Maximilian on 16.03.18.
+ */
+
+public class RidesFragment extends android.support.v4.app.Fragment{
+
+    /**
+     * The fragment argument representing the section number for this fragment.
+     */
+    public static final String ARG_SECTION_NUMBER = "Section";
+
+    /**
+     * Returns a new instance of this fragment for the given section number.
+     */
+    public static RidesFragment newInstance(int sectionNumber) {
+        RidesFragment fragment = new RidesFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public RidesFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_rides, container,
+                false);
+
+        //ActionBar
+        setHasOptionsMenu(true);
+
+/*
+        ImageView botImageView = (ImageView)rootView.findViewById(R.id.botImage);
+        botImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("fb-messenger://user/");
+                //long id = 2064511353834525;
+                uri = ContentUris.withAppendedId(uri,2064511353834525L);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+
+            }
+        });*/
+
+        return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+    }
+}
